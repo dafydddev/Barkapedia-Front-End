d5d3c0d15c2fafa07cf403f0dfa63a43b30c3ad5
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-06-15 23:13:59 +0100

    basic sign in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import Header from "./Components/Header"
+import { firebaseSignIn } from "../db/connection";
+import Header from "./Components/Header";
+
 function App() {
+  const email = "joe@example.com";
+  const password = "abc1232";
   return (
-    <>
+    <> 
+    {
+    firebaseSignIn(email, password)
+    }
       <Header />
     </>
   );
