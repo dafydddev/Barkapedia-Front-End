050f468cc22261e8c983aac629e4ef9d38385b6e
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-06-16 11:27:34 +0100

    added instructions to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import { firebaseSignIn } from "../db/connection";
 import Header from "./Components/Header";
 
 function App() {
+
+  // To get the below to work, make sure that you have the firebase emulator running and listen.ts running
+  // From there, go a POST to http://localhost:9191/api/users/ with the below ...
+  // {
+  //   "email": "joe@example.com",
+  //   "username": "JoeB",
+  //   "password": "abc1232",
+  //   "type": "consumer"
+  // }
+  // ... from after this, the auth error should change to be "signed in user_11"
+
   const email = "joe@example.com";
   const password = "abc1232";
   return (
